b9e3ddeed6325011ac5cf57eb0ece4efcd5e154c
Comdex <wcomdex@foxmail.com>
2015-08-24 11:37:53 +0800

    添加链表Insert通用实现

--- ds/sqlist.go
+++ ds/sqlist.go
@@ -41,11 +41,21 @@ func (this *SqList) Insert(index int, data interface{}) error {
 	if index < 0 || index > this.Length() {
 		return errors.New("The index is illegal!")
 	}
+	// 利用golang的切片基本操作来解决
 	var temp []interface{} = make([]interface{}, len(this.data[index:]))
 	copy(temp,this.data[index:])
 	current := append(this.data[:index],data)
 	this.data = append(current,temp...)
 	return nil
+	
+	// 利用通用的原始方法来解决，在如java的语言中顺序表利用数组来解决要预先分配足够
+	// 大的空间，把index下标及其后的元素后移后再插入新元素
+//	this.data = append(this.data,data) //先利用data为切片再增加一个元素的空间
+//	for i:=this.Length()-1; i>index; i-- {
+//		this.data[i] = this.data[i-1]
+//	}
+//	this.data[index] = data
+//	return nil
 }
 
 func (this *SqList) Remove(index int) error {
